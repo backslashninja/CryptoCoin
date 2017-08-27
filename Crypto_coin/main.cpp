a2d4036ac529b088347c3992ddcdbd89a7aa0356
backslashninja <31382394+backslashninja@users.noreply.github.com>
2017-08-27 17:32:08 +0530

    Update main.cpp

--- Crypto_coin/main.cpp
+++ Crypto_coin/main.cpp
@@ -5,6 +5,7 @@
 #include <string.h> //for strcpy and strcmp
 #include <windows.h>
 #include <ctime>
+#include <fstream>
 
 #include "user.h"
 #include "transaction.h"
@@ -20,6 +21,9 @@ char mk[20]="antriksh";
 void getkey(char [], int);
 
 void maketransaction(int ,int ,int );
+//void feedtransaction(char[],int,int,int,time_t);
+long int createpk(char[],int,int,int,time_t);
+bool verifypk(char[],int,int,int,long int,long int);
 
 
 
@@ -30,7 +34,8 @@ do
 {
     system("cls");
     cout << "\t\tWelcome to CryptoCoin \n\nWhat are you here for ?\n ";
-    cout <<"1. create new account \n 2. delete account\n 3. make transaction\n 4. display Account balances\n\t\t\t";
+    cout <<"1. create new account \n 2. delete account\n 3. make transaction";
+    cout<<"\n 4. Verify a transaction \n 5. display Account balances\n\t\t\t";
     cin>>x;
 
 
@@ -78,7 +83,21 @@ do
 
         }
         {
-            case 4 : user u1;
+            case 4 : int amt,peeno,perno; long int pk,timet;  char skey[20];
+                     cout<<"\nEnter the time of transaction  ";  cin>>timet;                 //will work till 2038;
+                     cout<<"\nEnter the account no. of the payer "; cin>>perno;
+                     cout<<"\nEnter the account no. of the payee "; cin>>peeno;
+                     cout<<"\nEnter the amount paid ";    cin>>amt;
+                     cout<<"\nEnter the digital signatures of the payer "; cin>>pk;
+                     cout<<"\nEnter the sectret key of the user  "; getkey(skey,20);
+
+                     if(verifypk(skey,perno,peeno,amt,timet ,pk ))
+                        cout<<"\nThis transaction was authorised by the system. \n";
+                     else cout<<"\nThis transaction  is FRAUD !!! \n";
+                     break;
+        }
+        {
+            case 5 : user u1;
                      u1.displayall();
                      break;
         }
@@ -91,21 +110,83 @@ do
 }
 
 void maketransaction(int perno, int peeno, int amt)
-{   user payer,payee;  char key[20],skey[20];
+{   time_t holdtime;
+    holdtime=time(NULL);
+    long int transtime=holdtime;
+    user payer,payee;  char key[20],skey[20],pername[50],peename[50];
     payer.searchuser(perno);  payee.searchuser(peeno);
     cout<<"\nThe Payer needs to enter his/her secret key for authorizing the transaction.. ";
     getkey(key,20);
     payer.givekey(skey);
+    payer.givename(pername);
+    payee.givename(peename);
     if(!strcmp(key,skey))
     {
         cout<<"\n TRANSACTION AUTHORIZED !!! \n";
         payee.updateuser(peeno,amt);
         payer.updateuser(perno,0-amt);
 
+        long int pk=createpk(skey,perno,peeno,amt,transtime);
+
+        cout<<"At time "<<transtime<<" ";
+        cout<<pername;
+        cout<<" pays ";
+        cout<<peename;
+        cout<<"an amount of ";
+        cout<<amt;
+        cout<<" Crypto coins. \nDigital Signature of Payer : ";
+        cout<<pk;
+        cout<<"\n\n";
+
+        ofstream fout;
+        fout.open("Transactions.txt",ios::out|ios::app);
+
+        fout<<"At time "<<transtime<<" ";
+        fout<<pername;
+        fout<<" pays ";
+        fout<<peename;
+        fout<<"an amount of ";
+        fout<<amt;
+        fout<<" Crypto coins. \nDigital Signature of Payer : ";
+        fout<<pk;
+        fout<<"\n\n";
+
+        fout.close();
+
     }
     else cout<<"You are not Recognised by the system.. ";
-
 }
+
+
+long int createpk(char skey[],int perno,int peeno,int amt,long int transtime)
+{
+    int i=0,num; char ch;
+    while(i>20)
+    {
+        ch=skey[i];
+        num += int(ch);
+        i++;
+    }
+
+    long int pk=(num + peeno+ transtime)*perno-35466644343;
+}
+
+bool verifypk(char skey[],int perno,int peeno,int amt,long int transtime,long int vpk)
+{
+    int i=0,num; char ch;
+    while(i>20)
+    {
+        ch=skey[i];
+        num += int(ch);
+        i++;
+    }
+
+    long int pk=(num + peeno+ transtime)*perno-35466644343;
+
+    if(pk==vpk) return true;
+    else return false;
+}
+
 
 
 
